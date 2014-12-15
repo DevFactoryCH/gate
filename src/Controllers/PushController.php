a57af49522b542fb9fd51b019b5b94fd072d60e3
Alcindo Da Costa <alcindo.dacosta@devfactory.ch>
2014-12-15 14:55:19 +0100

    Remove use of the package davibennun/laravel-push-notification and use Sly/NotificationPusher

--- src/Controllers/PushController.php
+++ src/Controllers/PushController.php
@@ -2,21 +2,53 @@
 namespace Devfactory\Gate\Controllers;
 
 use Devfactory\Gate\Models\Message;
+
+use Sly\NotificationPusher\PushManager,
+  Sly\NotificationPusher\Adapter\Gcm as GcmAdapter,
+  Sly\NotificationPusher\Collection\DeviceCollection,
+  Sly\NotificationPusher\Model\Device,
+  Sly\NotificationPusher\Model\Message as MessageSly,
+  Sly\NotificationPusher\Model\Push
+  ;
+
 use Redirect;
+use Config;
 
 class PushController extends \Devfactory\Gate\Controllers\GateController
 {
   public function send(Message $message)
   {
 
-    PushNotification::app('appNameIOS')
-      ->to($message->device->token)
-      ->send('Gate notify',
-             array(
-               'message' => $message->device->message,
-               'numbers' => $message->device->numbers,
-             )
-            );
+
+    if (Config::get('gate::production')) {
+      $env = PushManager::ENVIRONMENT_PROD;
+    } else {
+      $env = PushManager::ENVIRONMENT_DEV;
+    }
+
+    $pushManager = new PushManager($env);
+
+    $gcmAdapter = new GcmAdapter(
+      array(
+        'apiKey' => Config::get('gate::api_key'),
+      ));
+
+    $devices = new DeviceCollection(
+      array(
+        new Device($message->device->token)
+      )
+    );
+
+    $messageApp = new MessageSly(
+      array(
+        'message' => $message->device->message,
+        'numbers' => $message->device->numbers,
+      )
+    );
+
+    $push = new Push($gcmAdapter, $devices, $messageApp);
+    $pushManager->add($push);
+    $pushManager->push();
 
     $message->sent = true;
     $message->save();
